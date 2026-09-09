f57e323d12f3af89b139842997b2d77e900738b6
CrazyPablo <71659677+CrazyPablo@users.noreply.github.com>
2026-09-09 12:52:27 +0200

    feat: add Application Service

---
+++ RecruitmentTask/Service/ApplicationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecruitmentTask.Client;
+using RecruitmentTask.File;
+
+namespace RecruitmentTask.Service
+{
+    public class ApplicationService(
+        IApiClient apiClient,
+        IFileWriter fileWriter)
+    {
+        private readonly IApiClient _apiClient = apiClient;
+        private readonly IFileWriter _fileWriter = fileWriter;
+
+        public async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var data = await _apiClient.GetDataAsync(cancellationToken);
+
+            await _fileWriter.AppendAsync(data, cancellationToken);
+        }
+    }
+}
