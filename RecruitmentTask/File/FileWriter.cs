9619840c7aed7849cc33ff008e2197086c13d5c7
CrazyPablo <71659677+CrazyPablo@users.noreply.github.com>
2026-09-09 12:42:37 +0200

    feat: add File Writer

---
+++ RecruitmentTask/File/FileWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using RecruitmentTask.Model;
+
+namespace RecruitmentTask.File
+{
+    public class FileWriter(string filePath) : IFileWriter
+    {
+        private readonly string _filePath = filePath;
+
+        public async Task AppendAsync(CatFact data, CancellationToken cancellationToken)
+        {
+            string json = JsonSerializer.Serialize(data);
+
+            await System.IO.File.AppendAllTextAsync(_filePath, json + Environment.NewLine, cancellationToken);
+        }
+    }
+}
---
+++ RecruitmentTask/File/IFileWriter.cs
@@ -0,0 +1,9 @@
+﻿using RecruitmentTask.Model;
+
+namespace RecruitmentTask.File
+{
+    public interface IFileWriter
+    {
+        Task AppendAsync(CatFact data, CancellationToken cancellationToken);
+    }
+}
